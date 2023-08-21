a92c9721f66ac117b802227835e53603f8ea7aa1
Shivam Kumar <shivam.kumar77087@gmail.com>
2023-08-21 23:39:35 +0530

    add cart solved

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -63,4 +63,12 @@ export class ProductService {
     return this.http.post('http://localhost:3000/cart',cartData)
     alert(cartData)
   }
+  getCartList(userId:number){
+    return this.http.get<product[]>('http://localhost:3000/cart?userId='+userId,
+    {observe:'response'}).subscribe((result)=>{
+      //console.log(result);
+      if(result && result.body)
+      this.cartData.emit(result.body)
+    })
+  }
 } 
